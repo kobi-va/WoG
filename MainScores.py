64580232709427e0a814a0fff297e259cf8d30f9
kobi-va <93861348+kobi-va@users.noreply.github.com>
2022-03-08 13:00:51 +0200

    Update MainScores.py

--- MainScores.py
+++ MainScores.py
@@ -6,28 +6,18 @@ import sys
 linux_home = (os.path.expanduser('~'))
 
 file = Utils.score_file()
-app = Flask(__name__, template_folder="./")
+app = Flask(__name__, template_folder="/")
 
 
 @app.route("/")
 def score_server():
-    if os.name == "nt":
-        try:
-            f = open(file, "r")
-            score = f.read()
-            return render_template("score.html", score=score)
-            f.close()
-        except:
-            return render_template("error.html", Error="Error")
-
-    else:
-        try:
-            f = open(f"{linux_home}/WoG_Project/{file}", "r")
-            score = f.read()
-            return render_template("score.html", score=score)
-            f.close()
-        except:
-            return render_template("error.html", Error="Error")
+    try:
+        f = open(file, "r")
+        score = f.read()
+        return render_template("score.html", score=score)
+        f.close()
+    except:
+        return render_template("error.html", Error="Error")
 
 
 sys.stdout = open(os.devnull, "w")
