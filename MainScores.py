840a544127db559bbb7be65ac397dd39786b433e
kobi-va <93861348+kobi-va@users.noreply.github.com>
2022-03-08 12:44:04 +0200

    Update MainScores.py

--- MainScores.py
+++ MainScores.py
@@ -6,10 +6,7 @@ import sys
 linux_home = (os.path.expanduser('~'))
 
 file = Utils.score_file()
-if os.name == "nt":
-    app = Flask(__name__, template_folder="../WoG_Project")
-else:
-    app = Flask(__name__, template_folder=f"{linux_home}/WoG_Project")
+app = Flask(__name__, template_folder="./")
 
 
 @app.route("/")
