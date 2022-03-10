f9bb96886d9d4215bdcbeec185f1bc35b130a6f3
kobi-va <93861348+kobi-va@users.noreply.github.com>
2022-03-10 14:42:44 +0200

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -3,14 +3,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import sys
+import os
 
+
+os.chmod('./chromedriver', 0755) 
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-using")
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
-browser = webdriver.Chrome(executable_path="./chromedriver", options=chrome_options)
+browser = webdriver.Chrome(executable_path='./chromedriver', options=chrome_options)
 
 
 def test_scores_service():
